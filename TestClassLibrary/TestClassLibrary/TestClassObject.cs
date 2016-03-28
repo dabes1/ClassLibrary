bf738d2ffc0d650c1eae2ca5d3438a77eeebbdb9
Darrell Abes <dabes1@hotmail.com>
2016-03-28 11:13:54 -0700

    Initial Release

---
+++ TestClassLibrary/TestClassLibrary/TestClassObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassLibraryNamespace
+{
+    public class TestClassObject
+    {
+        private static string _clientID = "TEST_CLIENT_ID";
+        private static string _secretID = "TEST_SECRET_ID";
+
+        public static string GetClientID { get { return _clientID; } }
+        public static string GetSecretID {  get { return _secretID; } }
+    }
+}
